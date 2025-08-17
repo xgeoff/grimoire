eaf57e0e9bda612c68d89e2f6d60c86aa12948a1
xgeoff <xgeoff@gmail.com>
2025-08-16 20:57:50 -0700

    open and close helpers

---
+++ plugin/src/main/resources/scaffold/basic/helpers/close.groovy
@@ -0,0 +1,12 @@
+package scaffold.basic.helpers
+
+import com.github.jknack.handlebars.Helper
+import com.github.jknack.handlebars.Options
+
+return { Object tagArg, Options options ->
+    String tag = String.valueOf(tagArg ?: "").trim()
+    String cls = (opts.hash("class") ?: "").toString().trim()
+    String classAttr = cls ? " class=\"${handlebars.escapeExpression(cls)}\"" : ""
+
+    new Handlebars.SafeString("</${tag}>")
+} as Helper
