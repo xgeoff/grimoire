65f18b62e3f22eedfd495e883b32c13acf9f5a08
Jefe <xgeoff@gmail.com>
2025-08-19 08:29:29 -0700

    fix: ensure sidebar links use .html extension

--- plugin/src/test/groovy/biz/digitalindustry/grimoire/partial/SidebarItemPartialSpec.groovy
+++ plugin/src/test/groovy/biz/digitalindustry/grimoire/partial/SidebarItemPartialSpec.groovy
@@ -9,9 +9,47 @@ class SidebarItemPartialSpec extends Specification {
         def handlebars = new Handlebars()
         def eqHelper = new GroovyShell().evaluate(new File('src/main/resources/scaffold/basic/helpers/eq.groovy'))
         handlebars.registerHelper('eq', eqHelper)
+        def htmlPathHelper = new GroovyShell().evaluate(new File('src/main/resources/scaffold/basic/helpers/htmlPath.groovy'))
+        handlebars.registerHelper('htmlPath', htmlPathHelper)
         def partial = new File('src/main/resources/scaffold/basic/partials/sidebarItem.hbs').text
         def template = handlebars.compileInline(partial)
         def context = [baseUrl: '/test', type: 'file', name: 'index', path: 'index']
+
+        when:
+        def output = template.apply(context)
+
+        then:
+        output.contains('<a href="/test/index.html">index</a>')
+    }
+
+    def "strips non-html extensions"() {
+        given:
+        def handlebars = new Handlebars()
+        def eqHelper = new GroovyShell().evaluate(new File('src/main/resources/scaffold/basic/helpers/eq.groovy'))
+        handlebars.registerHelper('eq', eqHelper)
+        def htmlPathHelper = new GroovyShell().evaluate(new File('src/main/resources/scaffold/basic/helpers/htmlPath.groovy'))
+        handlebars.registerHelper('htmlPath', htmlPathHelper)
+        def partial = new File('src/main/resources/scaffold/basic/partials/sidebarItem.hbs').text
+        def template = handlebars.compileInline(partial)
+        def context = [baseUrl: '/test', type: 'file', name: 'README', path: 'README.md']
+
+        when:
+        def output = template.apply(context)
+
+        then:
+        output.contains('<a href="/test/README.html">README</a>')
+    }
+
+    def "keeps html extension"() {
+        given:
+        def handlebars = new Handlebars()
+        def eqHelper = new GroovyShell().evaluate(new File('src/main/resources/scaffold/basic/helpers/eq.groovy'))
+        handlebars.registerHelper('eq', eqHelper)
+        def htmlPathHelper = new GroovyShell().evaluate(new File('src/main/resources/scaffold/basic/helpers/htmlPath.groovy'))
+        handlebars.registerHelper('htmlPath', htmlPathHelper)
+        def partial = new File('src/main/resources/scaffold/basic/partials/sidebarItem.hbs').text
+        def template = handlebars.compileInline(partial)
+        def context = [baseUrl: '/test', type: 'file', name: 'index', path: 'index.html']
 
         when:
         def output = template.apply(context)
